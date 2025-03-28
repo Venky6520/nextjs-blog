3724cab6ce851a9fc4d7e6a4d8f067eb650c404b
Venkatesh kumar <167079967+Venky6520@users.noreply.github.com>
2025-03-28 19:19:28 +0530

    initial commit , navigating between pages, setup pages,link component,cllient side navigation, layout component,assests,metadata

---
+++ pages/posts/first-post.js
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import Layout from '../../components/layout';
+export default function FirstPost() {
+  return (
+    <>
+      <Layout>
+      <Head>
+  <title>First Post</title>
+  <script src="https://connect.facebook.net/en_US/sdk.js" />
+</Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">← Back to home</Link>
+      </h2>
+      <Image
+    src="/images/profile.jpg" // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt="Your Name"
+  />
+    </Layout>
+    </>
+  );
+}
