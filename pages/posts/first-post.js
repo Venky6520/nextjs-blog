9daa7a570eeb8c894b83e08f8133a5d41955c65f
Venkatesh kumar <167079967+Venky6520@users.noreply.github.com>
2025-04-01 20:57:01 +0530

    polishing layout, creating a simple blog architecture,prerendering

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -3,13 +3,21 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import Layout from '../../components/layout';
+import Script from 'next/script';
+
 export default function FirstPost() {
   return (
     <>
       <Layout>
       <Head>
   <title>First Post</title>
-  <script src="https://connect.facebook.net/en_US/sdk.js" />
+  <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
 </Head>
       <h1>First Post</h1>
       <h2>
